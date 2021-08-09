4e0773556c4609fe1852c67b416a02ca735287e9
Jayson Clark <jayson.clark0421@gmail.com>
2021-08-09 14:03:25 -0400

    Updated template file

--- src/templates/db_query.js
+++ src/templates/db_query.js
@@ -3,33 +3,21 @@ const format = require('pg-format');
 
 module.exports = (app, client) => {
 
-    const PATH = __filename.split('/routes')[1].split('.js')[0];
+    let PATH = __filename.split('/routes')[1];
+    PATH = PATH.split(PATH.includes('index.js') ? 'index.js' : '.js')[0];
 
     app.get(PATH, (req, res) => {
 
-        try {
+        const query = format('SELECT * FROM table;');
 
-            const query = format('');
+        client.query(query).then(result => {
 
-            client.query(query).then(result => {
-
-                return res.json({
-                    rows: result.rows,
-                });
-
+            return res.json({
+                rows: result.rows,
             });
 
-        } catch(err) {
-
-            console.log(err);
-
-            return res.status(500).json({
-                error: 'An internal server error has occured.',
-            });
-
-        }
+        });
 
     });
-
 
 }
