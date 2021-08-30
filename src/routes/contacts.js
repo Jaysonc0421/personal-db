02d49f0cc8f0c01afc7e978a2de06aa5748abaef
Jayson Clark <jayson.clark0421@gmail.com>
2021-08-29 22:28:17 -0400

    Added Error Checks
    When incorrect data type were provided to the POST and PATCH methods,
    errors that caused the program to stop execution occurred. Fixed all
    known issues relating to this. Also require that a contact's first name
    is not blank.

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -6,6 +6,7 @@ module.exports = (app, client) => {
     PATH = PATH.split(PATH.includes('index.js') ? 'index.js' : '.js')[0];
 
     const columns = ['first', 'middle', 'last', 'phones', 'emails', 'urls', 'socials', 'notes'];
+    const json_columns = ['phones', 'emails', 'urls', 'socials'];
 
     // Return a list of contacts
     app.get(PATH, (req, res) => {
@@ -22,11 +23,34 @@ module.exports = (app, client) => {
     app.post(PATH, (req, res) => {
 
         // Require that a first name be provided
-        if(!req.body.first) return res.status(400).json({
+        if(!('first' in req.body) || req.body.first.length === 0) return res.status(400).json({
             error: 'Required json: first',
         });
 
         const provided_columns = columns.filter(col => col in req.body);
+
+        // Determine if provided json columns are infact objects
+        for(const col of json_columns) {
+
+            const isObject = (typeof req.body[col] !== 'object');
+            const isArray = Array.isArray(req.body[col]);
+
+            if(provided_columns.includes(col) && (isObject || isArray)) return res.status(400).json({
+                error: `${ json_columns.join(', ') } must be a json object.`,
+            });
+
+        }
+
+        // If the provided value is not a json column, and is not provided as a string
+        // parse it as one to prevent incorrect datatype errors.
+        for(const col of provided_columns) {
+
+            const type = (typeof req.body[col]);
+
+            if(json_columns.includes(col) && type === 'string') req.body[col] = JSON.stringify(req.body[col]);
+
+        }
+
         const cols = provided_columns.join(', ');
         const values = provided_columns.map(col => format('%L', req.body[col])).join(', ');
 
@@ -49,7 +73,7 @@ module.exports = (app, client) => {
     // Modify an existing contact
     app.patch(PATH + '/:id', (req, res) => {
 
-        // A contact id is required
+        // The contact id must be parseable as an integer
         if(!parseInt(req.params.id)) return res.status(400).json({
             error: 'The contact id must be an integer.',
         });
@@ -59,6 +83,33 @@ module.exports = (app, client) => {
         if(provided_columns.length === 0) return res.status(400).json({
             error: 'Atleast one value must be modified.',
         });
+
+        // If the first name is updated, check if it is atleast one character in length
+        if('first' in req.body && req.body.first.length === 0) return res.status(400).json({
+            error: 'A contact must include a first name.',
+        }); 
+
+        // Determine if provided json columns are infact objects
+        for(const col of json_columns) {
+
+            const isObject = (typeof req.body[col] !== 'object');
+            const isArray = Array.isArray(req.body[col]);
+
+            if(provided_columns.includes(col) && (isObject || isArray)) return res.status(400).json({
+                error: `${ json_columns.join(', ') } must be a json object.`,
+            });
+
+        }
+
+        // If the provided value is not a json column, and is not provided as a
+        // string, parse it as one to prevent incorrect datatype errors.
+        for(const col of provided_columns) {
+
+            const type = (typeof req.body[col]);
+
+            if(json_columns.includes(col) && type === 'string') req.body[col] = JSON.stringify(req.body[col]);
+
+        }
 
         const values = provided_columns.map(col => format('%I = %L', col, req.body[col]));
         const query = format(`UPDATE contacts SET ${ values } WHERE id = %L RETURNING *;`, parseInt(req.params.id));
@@ -80,7 +131,7 @@ module.exports = (app, client) => {
     // Delete an existing contact
     app.delete(PATH + '/:id', (req, res) => {
 
-        // A contact id is required
+        // The contact id must be parseable as an integer
         if(!parseInt(req.params.id)) return res.status(400).json({
             error: 'The contact id must be an integer.',
         });
